336c365f33420f86a86f775329cac5189669988d
cassiano <cassianoleal@gmail.com>
2013-07-09 11:28:17 -0300

    Add 'supports' metadata

--- metadata.rb
+++ metadata.rb
@@ -6,5 +6,8 @@ description      "Installs and configures a system-wide Ruby, with the option of
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
+supports 'ubuntu'
+supports 'debian'
+
 depends 'apt'
 depends 'build-essential'
